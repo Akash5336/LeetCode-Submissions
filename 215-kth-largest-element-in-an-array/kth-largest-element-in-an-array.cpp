05400b44ad64b5509ead82ab0146464bf6a6d4b0
Akash P <44171593+Akash5336@users.noreply.github.com>
2023-08-14 22:02:34 +0530

    Time: 196 ms (6.85%) | Memory: 45.4 MB (86.52%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,23 +1,51 @@
 class Solution {
 public:
+int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[left];
+        int l = left + 1;
+        int r = right;
+
+        while (l <= r) {
+            if (nums[l] < pivot && nums[r] > pivot) {
+                swap(nums[l++], nums[r--]);
+            }
+            if (nums[l] >= pivot) {
+                l++;
+            }
+            if (nums[r] <= pivot) {
+                r--;
+            }
+        }
+
+        swap(nums[left], nums[r]);
+        return r;
+    }
     int findKthLargest(vector<int>& nums, int k) {
-        // int max=0;
-        // set<int> test;
-    //     for(auto i: nums){
-    //         test.insert(i);
-    //     }
+    //     int max=0;
     //    for(int i=0;i<k;i++){
-    //         max = *max_element(test.begin(),test.end());
-    //         auto it = find(test.begin(), test.end(), max);
-    //         if (it != test.end()) {
-    //             test.erase(it);
+    //         max = *max_element(nums.begin(),nums.end());
+    //         auto it = find(nums.begin(), nums.end(), max);
+    //         if (it != nums.end()) {
+    //             nums.erase(it);
     //         }
     //    }
-    //     for(auto i: test){
-    //         cout<<i;
-    //     }
     //    return max;
-    sort(nums.begin(),nums.end());
-    return nums[nums.size()-k];
+    // sort(nums.begin(),nums.end());
+    // return nums[nums.size()-k];
+    int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int pivotIndex = partition(nums, left, right);
+            if (pivotIndex == k - 1) {
+                return nums[pivotIndex];
+            } else if (pivotIndex < k - 1) {
+                left = pivotIndex + 1;
+            } else {
+                right = pivotIndex - 1;
+            }
+        }
+
+        return -1;
     }
 };
