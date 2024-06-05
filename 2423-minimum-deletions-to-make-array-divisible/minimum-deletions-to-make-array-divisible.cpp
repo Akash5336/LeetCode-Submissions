5d15b33dc16a208f93369cec00aa2425fe812889
Akash P <44171593+Akash5336@users.noreply.github.com>
2024-06-05 10:18:34 +0530

    Time: 97 ms (94.27%) | Memory: 91.5 MB (77.53%) - LeetSync

--- 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.cpp
+++ 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.cpp
@@ -1,31 +1,26 @@
 class Solution {
-private:
-    int gcd(int a,int b){
-        while(b>0){
-            int tmp=a;
-            a=b;
-            b=tmp%b;
-        }
-        return a;
-    }
 public:
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
         int g = numsDivide[0];
         for(auto i:numsDivide){
-            g = gcd(g,i);
+            g = __gcd(g,i);
         }
-        int smallest = INT_MAX;
-        for(auto num:nums){
-            if(g%num==0)
-                smallest = min(smallest,num);
-        }
-        if(smallest==INT_MAX)
-            return -1;
-        int minOp=0;
-        for(auto num:nums){
-            if(num<smallest)
-                ++minOp;
-        }
-        return minOp;
+        // int smallest = INT_MAX;
+        // for(auto num:nums){
+        //     if(g%num==0)
+        //         smallest = min(smallest,num);
+        // }
+        // if(smallest==INT_MAX)
+        //     return -1;
+        // int minOp=0;
+        // for(auto num:nums){
+        //     if(num<smallest)
+        //         ++minOp;
+        // }
+        // return minOp;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+            if(g%nums[i]==0) return i;
+        return -1;
     }
 };
