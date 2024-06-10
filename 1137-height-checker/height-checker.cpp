0f5fe51d590300bd2a445bb22a7b4b23cb587d3c
Akash P <44171593+Akash5336@users.noreply.github.com>
2024-06-10 21:22:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (10.73%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> sorted(heights.size());
-        partial_sort_copy(begin(heights), end(heights), begin(sorted), end(sorted));
+        vector<int> sorted = heights;
+        sort(sorted.begin(),sorted.end());
         int ans=0;
         for(int i=0;i<heights.size();i++){
             if(heights[i]!=sorted[i])
