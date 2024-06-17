17226eef5254855dc33b42cde8e1ad377f31386a
Akash P <44171593+Akash5336@users.noreply.github.com>
2024-06-17 21:35:46 +0530

    Time: 2 ms (70.95%) | Memory: 7.1 MB (61.35%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool judgeSquareSum(int c) {
         unsigned l=0,r=sqrt(c);
+        if(r*r==c)
+            return true;
         while(l<=r){
             const unsigned sum=l*l+r*r;
             if(sum==c)
