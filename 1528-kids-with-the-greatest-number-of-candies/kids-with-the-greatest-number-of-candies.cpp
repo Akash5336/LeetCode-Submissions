5f5a178814e1f28cdc9f04f9c59cc6970c9aec63
Akash P <44171593+Akash5336@users.noreply.github.com>
2024-07-12 10:59:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (30.62%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max = *max_element(candies.begin(),candies.end());
+        vector<bool> ans;
+
+        for(const int candy:candies){
+            if(candy+extraCandies >= max)
+                ans.push_back(true);
+            else
+                ans.push_back(false);
+        }
+        return ans;
+    }
+};
